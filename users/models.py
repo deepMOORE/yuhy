d612474b5fc0a5e79ddddc1c6297a466afd445c3
Vlad <vlad.puzin@gmail.com>
2020-02-09 22:52:11 +0300

    added password and email field to user

--- users/models.py
+++ users/models.py
@@ -5,5 +5,7 @@ class User(models.Model):
     id = models.AutoField(primary_key=True)
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
+    email = models.CharField(max_length=255)
+    password = models.CharField(max_length=255)
     country_id = models.PositiveIntegerField()
     created_at = models.DateTimeField(auto_now_add=True)
