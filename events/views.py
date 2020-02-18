331acff47c1d76dd828d230c2422acf7a9d8326f
vlad.puzin@gmail.com <vlad.puzin@gmail.com>
2020-02-18 22:01:15 +0300

    added events-create endpoint

--- events/views.py
+++ events/views.py
@@ -1,7 +1,7 @@
 from rest_framework import viewsets, status, permissions
+from rest_framework.exceptions import ValidationError
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
-
 from .models import Event
 from .serializers import EventSerializer
 
@@ -21,4 +21,24 @@ class EventView(viewsets.ModelViewSet):
             'message': None,
             'data': serializer.data
         }, status=status.HTTP_200_OK)
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data)
+
+        try:
+            serializer.is_valid(raise_exception=True)
+        except ValidationError as e:
+            return Response({
+                'status': 'failed',
+                'message': e.detail,
+                'data': None,
+            }, status=status.HTTP_201_CREATED)
+
+        serializer.save()
+
+        return Response({
+            'status': status.HTTP_200_OK,
+            'message': None,
+            'data': None,
+        }, status=status.HTTP_201_CREATED)
 
