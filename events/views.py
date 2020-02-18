111240d66da2a80ffa2396365184f6944c7ecb7d
deepMOORE <vlad.puzin@gmail.com>
2020-02-18 20:24:14 +0300

    added events get-all endpoint

--- events/views.py
+++ events/views.py
@@ -1,4 +1,5 @@
-from rest_framework import viewsets, status
+from rest_framework import viewsets, status, permissions
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
 from .models import Event
@@ -6,8 +7,18 @@ from .serializers import EventSerializer
 
 
 class EventView(viewsets.ModelViewSet):
+    permission_classes = (IsAuthenticated, )
     queryset = Event.objects.all()
     serializer_class = EventSerializer
 
-    def create(self, request, *args, **kwargs):
-        return Response(data=None, status=status.HTTP_200_OK)
+    def list(self, request, *args, **kwargs):
+        serializer = self.serializer_class(self.queryset, many=True)
+
+        # todo: create enum with three statuses
+        # todo: create response model
+        return Response(data={
+            'status': status.HTTP_200_OK,
+            'message': None,
+            'data': serializer.data
+        }, status=status.HTTP_200_OK)
+
--- yuhy/urls.py
+++ yuhy/urls.py
@@ -3,15 +3,17 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
 from rest_framework_jwt.views import obtain_jwt_token, refresh_jwt_token, verify_jwt_token
-from authentication import views
+from authentication import views as auth_views
+from events import views as event_views
 
 router = routers.DefaultRouter()
-router.register('auth', views.RegistrationView)
+router.register('auth', auth_views.RegistrationView)
+router.register('events', event_views.EventView)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('events.urls')),
     url(r'^register/', include(router.urls)),
+    url(r'^get/', include(router.urls)),
 
     url(r'^api-token-auth/', obtain_jwt_token),
     url(r'^api-token-refresh/', refresh_jwt_token),
