5c439bb7b6899a12804b600ee3c3fc56ca5b19da
vlad.puzin@gmail.com <vlad.puzin@gmail.com>
2020-02-18 21:59:35 +0300

    main urls refactored

--- yuhy/urls.py
+++ yuhy/urls.py
@@ -1,19 +1,13 @@
 from django.conf.urls import url
-from django.contrib import admin
-from django.urls import path, include
-from rest_framework import routers
 from rest_framework_jwt.views import obtain_jwt_token, refresh_jwt_token, verify_jwt_token
-from authentication import views as auth_views
-from events import views as event_views
-
-router = routers.DefaultRouter()
-router.register('auth', auth_views.RegistrationView)
-router.register('events', event_views.EventView)
+from authentication.views import RegistrationView
+from events.views import EventView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    url(r'^register/', include(router.urls)),
-    url(r'^get/', include(router.urls)),
+    # path('admin/', admin.site.urls),
+    url(r'^events/get-list/', EventView.as_view({'get': 'list'})),
+
+    url(r'^user-register/', RegistrationView.as_view({'post': 'create'})),
 
     url(r'^api-token-auth/', obtain_jwt_token),
     url(r'^api-token-refresh/', refresh_jwt_token),
