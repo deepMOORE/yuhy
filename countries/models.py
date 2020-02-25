63aba207a451fa967a229f5e2006d6146aee1cc0
vpuzin <vlad.puzin@gmail.com>
2020-02-25 20:31:55 +0300

    removed countries app

--- countries/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-
-class Country(models.Model):
-    id = models.AutoField(primary_key=True)
-    name = models.PositiveIntegerField()
-    created_at = models.DateTimeField(auto_now_add=True)
