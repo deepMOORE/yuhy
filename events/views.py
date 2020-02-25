393cbb4676013a567ce4ae68ad1272daee51a84b
vpuzin <vlad.puzin@gmail.com>
2020-02-25 23:05:37 +0300

    added user's registration to event POST endpoint

--- events/views.py
+++ events/views.py
@@ -2,8 +2,8 @@ from rest_framework import viewsets, status, permissions
 from rest_framework.exceptions import ValidationError
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
-from .models import Event
-from .serializers import EventSerializer
+from .models import Event, EventUser
+from .serializers import EventSerializer, EventUserSerializer
 
 
 class GetEventsView(viewsets.ModelViewSet):
@@ -48,3 +48,29 @@ class CreateEventView(viewsets.ModelViewSet):
         }, status=status.HTTP_200_OK)
 
 
+class EventParticipationView(viewsets.ModelViewSet):
+    permission_classes = (IsAuthenticated,)
+    serializer_class = EventUserSerializer
+
+    def register(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            user_id = serializer.validated_data['user_id']
+            event_id = serializer.validated_data['event_id']
+
+            is_already_registered = EventUser.objects.filter(user_id=user_id, event_id=event_id)
+
+            if not is_already_registered:
+                serializer.save()
+
+                return Response({
+                    'status': 'success',
+                    'message': None,
+                    'data': None,
+                }, status=status.HTTP_200_OK)
+            else:
+                return Response({
+                    'status': 'failed',
+                    'message': 'User already participated in this event.',
+                    'data': None,
+                }, status=status.HTTP_400_BAD_REQUEST)
--- yuhy/urls.py
+++ yuhy/urls.py
@@ -3,13 +3,14 @@ from django.conf.urls import url
 # from django.urls import path
 from rest_framework_jwt.views import refresh_jwt_token, verify_jwt_token
 from authentication.views import RegistrationView, LoginReturnTokenView
-from events.views import GetEventsView, CreateEventView
+from events.views import GetEventsView, CreateEventView, EventParticipationView
 from users.views import GetUserView
 
 urlpatterns = [
     # path('admin/', admin.site.urls),
     url(r'^events/get-list/', GetEventsView.as_view({'get': 'list'})),
     url(r'^events/create-event/', CreateEventView.as_view({'post': 'create'})),
+    url(r'^events/register-user/', EventParticipationView.as_view({'post': 'register'})),
 
     url(r'^user/register/', RegistrationView.as_view({'post': 'create'})),
     url(r'^user/get-by-id/(?P<id>.+)/$', GetUserView.as_view({'get': 'get_by_id'})),
